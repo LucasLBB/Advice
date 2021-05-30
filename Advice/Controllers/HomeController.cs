a911a0d6dd325b525dffd61864139ffd349e2c2e
@GIS.LucasdeLima <lucasdelima@gestaointeligentesoftware.com.br>
2021-05-30 16:11:54 -0300

    Add project files.

---
+++ Advice/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using Advice.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Advice.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> MessageAdvice()
+        {
+            HttpClient httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri("https://api.adviceslip.com/");
+            HttpResponseMessage response = await httpClient.GetAsync("advice");
+
+            response.EnsureSuccessStatusCode();
+
+            var advice = response.Content.ReadAsStringAsync().Result;
+
+            ListAdvice listAdvice = JsonConvert.DeserializeObject<ListAdvice>(advice);
+
+            return View(listAdvice);
+        }
+
+    }
+}
---
+++ Advice/Models/AdviceSlip.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Advice.Models
+{
+
+    public class ListAdvice
+    {
+        public AdviceSlip slip { get; set; }
+    }
+
+    public class AdviceSlip
+    {
+        public int id { get; set; }
+
+        public string advice { get; set; }
+    }
+}
